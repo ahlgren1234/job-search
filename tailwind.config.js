479c5bf7baee2a90d8a4e89e4cc28edf8a540a8a
Peter Ahlgren <ahlgren1234@gmail.com>
2023-03-18 15:32:32 +0100

    navbar and tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "brand-gray-1": "#DADCE0",
+        "brand-blue-1": "#1967D2",
+        "brand-green-1": "#137333",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      margin: ["first"],
     },
   },
   plugins: [],
